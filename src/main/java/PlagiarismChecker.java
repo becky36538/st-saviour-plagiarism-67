90b363763d8623354fde76e17101ebb7d5ea0193
bmc2425 <recheriyan@stsaviour.org>
2026-03-19 22:50:30 -0400

    lab 6!

---
+++ src/main/java/PlagiarismChecker.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class PlagiarismChecker {
+    
+    public static void main(String[] args) {
+        try {
+            File originalFile = new File("poem.txt");
+            File plagarizedFile = new File("cheat.txt");
+
+            Scanner originalScanner = new Scanner(originalFile);
+            Scanner plagcanner = new Scanner(plagarizedFile);
+
+            List<String> originalLines = new ArrayList<>();
+            List<String> plagLines = new ArrayList<>();
+
+            while (organialScanner.hasNextLine()) {
+                originalLines.add(originalScanner.nextLine());
+            }
+
+            while (plagScanner.hasNextLine()) {
+                plagLines.add(plagScanner.nextLine());
+            }
+
+            originalScanner.close();
+            plagScanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: File not found.");
+            e.printStackTrace();;
+        }
+    }
+}
